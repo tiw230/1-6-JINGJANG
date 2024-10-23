0c15f2d2f067d1be9b30d0bd47ac9cdd7cd09897
tiw230 <tiw230230@gmail.com>
2024-10-23 14:17:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -36,12 +36,12 @@ module.exports = {
 
    // Lavalink Server Details
 
-  nodes: [
+   nodes: [
     {
-        name: "Working1",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        name: "Working2",
+        host: "lavalink.clxud.dev",
+        port: 2333,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
